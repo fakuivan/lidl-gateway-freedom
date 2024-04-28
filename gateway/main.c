571f73c1d618a522aa773df55f8fe969e24b2a70
fakuivan <fakuivan@gmail.com>
2024-04-28 17:02:33 -0300

    Fix segfault because of missing break (I love C)

--- gateway/main.c
+++ gateway/main.c
@@ -148,6 +148,7 @@ int main(int argc, char** argv)
                 break;
             case 'l':
                 loopback_only = true;
+                break;
             case 'b':
                 _serial_settings.baud_bps = atoi(optarg);
                 break;
